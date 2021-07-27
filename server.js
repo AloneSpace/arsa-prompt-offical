3cf045b2e4025de06f2496eb60803cd212d856e3
AloneSpace <jomcootle@gmail.com>
2021-07-27 18:07:36 +0700

    + Setup all routes

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//* Setup all routes
+require("./routes/routes")({ express, app });
+
 app.listen(PORT, () => {
     console.log(`Server listening in ${PORT}`);
 });
