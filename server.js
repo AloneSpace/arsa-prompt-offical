39e4128ef742d21decb087140a386ea93416a364
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-07-29 20:31:46 +0700

    + Update server.js setup view engine

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+var path = require('path');
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')));
 
 //* Setup all routes
 require("./routes/routes")({ express, app });
