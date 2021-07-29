490d9245223f7560e2cffb3a2d58c190e1272d32
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-07-29 22:23:15 +0700

    + Add Static files

---
+++ public/js/confirm.js
@@ -0,0 +1,27 @@
+$.urlParam = function(name){
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	return results[1] || 0;
+}
+
+$(document).ready(() => {
+    $("#confirmBtn").click(() => {
+        $.sweetModal.confirm('ยืนยันการสมัครอาสาสมัคร', 'ยืนยันหรือไม่', function () {
+            let userId = $.urlParam('id');
+            let uri_decoded = JSON.parse(decodeURI(userId));
+            let dataSet = {
+                name : $("#name").val(),
+                address : $("#address").val(),
+                phone : $("#tel").val(),
+                otherContact : $("#otherContact").val(),
+                province : $("#province").val(),
+                userId : uri_decoded
+            }
+            console.log(dataSet);
+
+            $.sweetModal('สมัครอาสาสำเร็จ !');
+
+        }, function () {
+            // $.sweetModal('You declined. That\'s okay!');
+        });
+    });
+})
