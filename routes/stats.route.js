a063d57cfa8afe2520ca4da6b41ea1195858e359
AloneSpace <jomcootle@gmail.com>
2021-07-29 13:58:47 +0700

    Edit path /v1/stats/getAll to /v1/stats

--- routes/stats.route.js
+++ routes/stats.route.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/stats.controller");
 module.exports = (express) => {
     const route = express.Router();
 
-    route.get("/getAll", controller.getAllStats);
+    route.get("/", controller.getAllStats);
 
     return route;
 };
