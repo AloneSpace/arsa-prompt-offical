83770a6be31336556314ef08cf8f61d9e2d94a13
AloneSpace <jomcootle@gmail.com>
2021-07-29 13:57:39 +0700

    + Add covidStats() methods for using in our project.

--- controllers/stats.controller.js
+++ controllers/stats.controller.js
@@ -1,25 +1,42 @@
-const axios = require('axios');
+const axios = require("axios");
 
-exports.getAllStats = function (req, res) {
-    axios.get("https://static.easysunday.com/covid-19/getTodayCases.json")
-        .then(result => {
-            result = result.data;
-            let data = {
-                "UpdateDate": result.UpdateDate,
-                "updated": result.updated, 
-                "cases": result.cases, 
-                "todayCases": result.todayCases,
-                "deaths": result.deaths, 
-                "todayDeaths": result.todayDeaths, 
-                "recovered": result.recovered, 
-                "todayRecovered": result.todayRecovered, 
-                "active": result.active, 
-                "critical": result.critical,
-                "credit" : result.DevBy
-            }
-            res.status(200).json(data);
-        })
-        .catch(err => {
-            console.log(err);
-        })
+async function getAllStats(req, res) {
+    try {
+        let stats = await covidStats();
+        return res.status(200).json(stats);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function covidStats() {
+    return new Promise((resolve, reject) => {
+        axios
+            .get("https://static.easysunday.com/covid-19/getTodayCases.json")
+            .then((result) => {
+                result = result.data;
+                let data = {
+                    UpdateDate: result.UpdateDate,
+                    updated: result.updated,
+                    cases: result.cases,
+                    todayCases: result.todayCases,
+                    deaths: result.deaths,
+                    todayDeaths: result.todayDeaths,
+                    recovered: result.recovered,
+                    todayRecovered: result.todayRecovered,
+                    active: result.active,
+                    critical: result.critical,
+                    credit: result.DevBy,
+                };
+                resolve(data);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+}
+
+module.exports = {
+    covidStats,
+    getAllStats,
 };
