159a4e25bccacb7ec0c6328ba40bec5d0274250b
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-07-29 11:33:50 +0700

    + fix : rename stats route

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
 module.exports = ({ express, app }) => {
     app.use("/v1/volunteers", require("./volunteer.route")(express));
-    app.use("/v1/getStats",require("./stats.route")(express))
+    app.use("/v1/stats",require("./stats.route")(express))
 };
