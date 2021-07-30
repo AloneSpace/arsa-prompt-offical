b09c55a283fdaeac0c150c712b74526d1b2d2d25
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-07-30 07:33:47 +0700

    + Add method findVolunteersById & fetchVolunteersById & Improve createVolunteers

--- controllers/volunteer.controller.js
+++ controllers/volunteer.controller.js
@@ -13,6 +13,12 @@ const findVolunteersByProvince = async function (req, res) {
     return res.status(200).json(volunteers);
 };
 
+const findVolunteersByUserId = async function (req, res) {
+    const volunteers = await await fetchVolunteersByUserId(req.body.userId);
+    if (!volunteers.length) return res.status(404).json("No Data Found");
+    return res.status(200).json(volunteers);
+}
+
 const createVolunteers = async function (req, res) {
     let reqData = req.body;
     if (
@@ -21,9 +27,17 @@ const createVolunteers = async function (req, res) {
         !reqData.name ||
         !reqData.otherContact ||
         !reqData.phone ||
-        !reqData.province
+        !reqData.province ||
+        !reqData.replyToken
     )
-        return res.status(400).json("Fill Form");
+        return res.status(200).json({
+            status: "error",
+            message: "Fill the form"
+        });
+    if ((await fetchVolunteersByUserId(reqData.userId)).length) return res.status(200).json({
+        status: "error",
+        message: "You're already volunteers"
+    })
     let data = {
         userId: req.body.userId,
         address: req.body.address,
@@ -37,9 +51,14 @@ const createVolunteers = async function (req, res) {
         await volunteersRef.add(data);
         return res.status(201).json({
             status: "success",
+            message: "Create Success"
         });
     } catch (err) {
         console.log(err);
+        return res.status(400).json({
+            status: "error",
+            message: "Something wrong"
+        });
     }
 };
 
@@ -55,6 +74,16 @@ const fetchVolunteers = async function () {
 
 const fetchVolunteersByProvince = async function (province) {
     const docs = await volunteersRef.where("province", "==", province).get();
+    if (docs.empty) return [];
+    let tempData = [];
+    docs.forEach((doc) => {
+        tempData.push(doc.data());
+    });
+    return tempData;
+};
+
+const fetchVolunteersByUserId = async function (userId) {
+    const docs = await volunteersRef.where("userId", "==", userId).get();
     if (docs.empty) return [];
     let tempData = [];
     docs.forEach((doc) => {
