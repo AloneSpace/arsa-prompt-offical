8cb1c05f10d50d8428db889c5995ca827fbe51d3
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-07-31 08:17:15 +0700

    + Add Edit Profile page & script

---
+++ public/js/update.js
@@ -0,0 +1,52 @@
+$(document).ready(() => {
+    $("#confirmBtn").click(() => {
+        Swal.fire({
+            title: 'ยืนยันการแก้ไขข้อมูล',
+            text: "คุณต้องการยืนยันการแก้ไขหรือไม่",
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#dba01f',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'ยืนยัน',
+            cancelButtonText: 'ปิด'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let secret = $("#secret").val();
+                let dataSet = {
+                    name: $("#name").val(),
+                    address: $("#address").val(),
+                    phone: $("#tel").val(),
+                    otherContact: $("#otherContact").val(),
+                    province: $("#province").val(),
+                }
+                console.log(dataSet);
+                axios.post(`/v1/volunteers/update/${secret}`, dataSet)
+                    .then(res => {
+                        console.log(res.data);
+
+                        if (res.status == 201) return Swal.fire({
+                            icon: 'success',
+                            title: 'สำเร็จ !',
+                            text: 'การแก้ไขข้อมูลสำเร็จ',
+                            confirmButtonColor: '#dba01f',
+                        })
+                        return Swal.fire({
+                            icon: 'error',
+                            title: 'ไม่สำเร็จ',
+                            text: res.data.message,
+                            confirmButtonColor: '#dba01f',
+                        })
+                    })
+                    .catch(err => {
+                        console.log(err.response.data);
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'ไม่สำเร็จ',
+                            text: err.response.data.message,
+                            confirmButtonColor: '#dba01f',
+                        })
+                    })
+            }
+        })
+    });
+})
