7e3a631a38836319f975758a019390782d190f4c
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-07-31 08:19:30 +0700

    + Add Editprofile page in pagecontroller

--- controllers/pages.controller.js
+++ controllers/pages.controller.js
@@ -1,4 +1,5 @@
 const { provinces } = require('../config/thailand');
+const { fetchVolunteersBySecretId } = require("../controllers/volunteer.controller");
 let index = async (req, res) => {
     res.render('index');
 }
@@ -11,7 +12,7 @@ let register = async (req, res) => {
             Buffer.from(decodeURI(data), "hex").toString()
         );
         // console.log(uri_decoded);
-        res.render('register', { provinces: provinces, data: { ...uri_decoded }, uri : req.params.data });
+        res.render('register', { provinces: provinces, data: { ...uri_decoded }, uri: req.params.data });
     } catch (err) {
         console.log(err);
         res.status(404).send("Error 404");
@@ -19,7 +20,25 @@ let register = async (req, res) => {
 
 }
 
+let editprofile = async (req, res) => {
+    let secretId = req.params.secretId;
+    if (!secretId) return res.status(400).json({
+        "status": "error",
+        "message": "Bad request"
+    })
+    try {
+        let userdata = await fetchVolunteersBySecretId(secretId);
+        if (!userdata.length) return res.status(400).json({
+            "status": "error",
+            "message": "User Not Found"
+        })
+        res.render('editprofile', { provinces: provinces, data: userdata[0].data });
+    } catch (err) {
+        res.status(404).send("Something Wrong");
+    }
+}
 module.exports = {
     index,
-    register
+    register,
+    editprofile
 }
