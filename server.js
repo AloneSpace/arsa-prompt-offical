3f8479b9fbe96e8e3f8edf678f036c40932f98d9
Karunpat <jomcootle@gmail.com>
2021-07-31 12:04:49 +0700

    + Ready for production

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-var path = require('path');
+var path = require("path");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
@@ -8,9 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug')
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 //* Setup all routes
 require("./routes/routes")({ express, app });
